84e948a522c6a0a0da2becae14735ec60adc8721
Olaf Fermum <olaf.fermum@gmail.com>
2025-02-09 19:03:40 -0800

    Refactored dynamic link creation.

--- js/main.js
+++ js/main.js
@@ -1,36 +1,48 @@
 /* globals MYPREZ, gtag */
 (function() {
   const birthdayInput = document.getElementById('birthday'),
-        wikipediaImageLink = document.getElementById('wikipedia-image-link'),
-        wikipediaLink = document.getElementById('wikipedia-link'),
-        pic = document.getElementById('pic'),
+        wikipediaLinks = document.getElementById('wikipedia-links'),
         result = document.getElementById('result');
+
+  let wikipediaImageLink,
+      wikipediaLink,
+      pic;
 
   function onDateChange() {
     if (birthdayInput.value) {
       const birthdayTime = new Date(birthdayInput.value).getTime(),
             administration = MYPREZ.findAdministration(birthdayTime);
 
+      wikipediaLinks.replaceChildren();  // Delete children
+
       if (administration) {
+        wikipediaImageLink = document.createElement('a');
         wikipediaImageLink.href = administration.href;
+
+        pic = document.createElement('img');
         pic.src = 'img/' + administration.image;
         pic.alt = administration.president;
+
+        wikipediaImageLink.appendChild(pic);
+
+        wikipediaLink = document.createElement('a');
         wikipediaLink.href = administration.href;
-        wikipediaLink.innerText = 'Wikipedia: ' + administration.president;
+        wikipediaLink.id = 'wikipedia-link';
+        wikipediaLink.textContent = 'Wikipedia: ' + administration.president;
+
+        wikipediaLinks.appendChild(wikipediaImageLink);
+        wikipediaLinks.appendChild(wikipediaLink);
+
         result.className = '';
-        result.innerText = 'You are a child of the ' + administration.administration + ' administration!';
+        result.textContent = 'You are a child of the ' + administration.administration + ' administration!';
 
         gtag('event', 'select_date', {
           'president': administration.administration
         });
       } else {
-        wikipediaImageLink.href = '';
-        pic.src = '';
-        pic.alt = '';
-        wikipediaLink.href = '';
-        wikipediaLink.innerText = '';
         result.className = 'error';
         result.innerText = 'Could not determine administration. Only dates after 1900 are supported.';
+
         gtag('event', 'error', {
           'type': 'Administration not found',
           'date': birthdayInput.value
